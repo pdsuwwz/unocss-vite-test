9d015b7aa431a1648c0c1cb2e015107f6d23341d
Wisdom <pdsu.wwz@foxmail.com>
2024-06-05 15:27:53 +0800

    feat: init repo

---
+++ uno.config.ts
@@ -0,0 +1,16 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  transformerDirectives
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives()
+  ],
+})
